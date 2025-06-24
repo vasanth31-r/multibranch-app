928e1ae95b61606321572a366cf51c5b1b8e1339
vasanth31-r <31vasanth02@gmail.com>
2025-06-24 16:22:13 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { add } = require('./math');
 
 const app = express();
-const PORT = 3000;
+const PORT = 6666;
 
 app.get('/', (req, res) => {
   res.send(`Hello from branch ${process.env.BRANCH_NAME}!`);
